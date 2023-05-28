1d7c4a23735cce45fc5d93531dfe21f8e0d1f4e0
zohar <hunji66@gmail.com>
2023-05-28 23:37:25 +0800

    custo

--- lua/my_plugins/example.lua
+++ lua/my_plugins/example.lua
@@ -7,4 +7,18 @@ return {
 
 	-- Your Plugin2
 	--  {'hardhacker/plugin2'},
+	{
+        "folke/which-key.nvim",
+        config = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+            require("which-key").setup({
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            })
+        end,
+    },
+    { "typicode/bg.nvim", lazy = false }
+
 }
